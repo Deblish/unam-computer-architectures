615d94241c2bec45b42eac4b8a9eb5a04f1dcd30
Aarón Padilla <samir_castillo@hotmail.com>
2020-10-29 10:39:23 -0600

    final registro
    pequenas correcciones

--- practica4/registro.vhd
+++ practica4/registro.vhd
@@ -9,23 +9,23 @@ ENTITY registro IS
 	PORT(
 		clk : IN std_logic;
 		reset : IN std_logic;
-		data_in : IN std_logic_vector(2 DOWNTO 0); --liga
-		data_out: OUT std_logic_vector(2 DOWNTO 0) --estado presente
+		liga : IN std_logic_vector(2 DOWNTO 0); --data_in
+		estadopresente : OUT std_logic_vector(2 DOWNTO 0) --data_out
 	);
 END registro;
 
 ARCHITECTURE behavioral OF registro IS
 	SIGNAL internal_value : std_logic_vector(2 DOWNTO 0) := B"000"; --ceros
 BEGIN
-	PROCESS (clk, reset, data_in) BEGIN
+	PROCESS (clk, reset, liga) BEGIN
 		IF reset = '1' THEN
 			internal_value <= B"000"; --inicias en ceros
 		ELSIF rising_edge(clk) THEN
-			internal_value <= data_in; --liga a auxiliar
+			internal_value <= liga; --liga a auxiliar
 		END IF;
 	END PROCESS;
 
 	PROCESS(internal_value) BEGIN
-		data_out <= internal_value; --auxiliar a estado presente
+		estadopresente <= internal_value; --auxiliar a estado presente
 	END PROCESS;
 END behavioral;
